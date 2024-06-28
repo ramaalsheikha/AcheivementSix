a0dbb002fdabc4617fc9310ad6684d613db2c588
rama <alsheikharama@gmail.com>
2024-06-28 17:40:37 +0300

    Improving API achievements on Project api.

--- data/src/main/java/com/example/acheivementsix/data/api/IProjectApi.kt
+++ data/src/main/java/com/example/acheivementsix/data/api/IProjectApi.kt
@@ -1,6 +1,9 @@
 package com.example.acheivementsix.data.api
 
+import retrofit2.http.GET
+
 interface IProjectApi {
+    @GET("/achievements")
     fun getAchievements(){
 
     }
---
+++ data/src/main/java/com/example/acheivementsix/data/repository/Repository.kt
@@ -0,0 +1,5 @@
+package com.example.acheivementsix.data.repository
+
+class Repository {
+
+}
